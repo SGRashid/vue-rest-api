d13cf681ecc608512beb5fe19d17b1482e3654e8
sgabdullin <sergey.gabdullin@centre-it.com>
2020-07-06 18:51:44 +0500

    develop: прописал отображение списка

--- client/frontend.js
+++ client/frontend.js
@@ -1,3 +1,23 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js';
 
-console.log(Vue);
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            form: {
+                name: '',
+                value: ''
+            },
+            contacts: []
+        }
+    },
+    methods: {
+        createContact() {
+            const {...contact} = this.form;
+            console.log(contact);
+            this.contacts.push({name: this.form.name, value: this.form.value});
+
+            this.form.name = this.form.value = '';
+        }
+    }
+});
